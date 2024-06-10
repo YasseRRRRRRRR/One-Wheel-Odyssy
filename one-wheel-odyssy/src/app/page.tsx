227464eb66a63c3040720989b75153a0ea099232
YasseRRRRRRRR <yassermoussaddak3@gmail.com>
2024-06-10 22:10:00 +0300

    got the avalanche working thank god

--- one-wheel-odyssy/src/app/page.tsx
+++ one-wheel-odyssy/src/app/page.tsx
@@ -1,113 +1,255 @@
+import MaxWidthWrapper from "@/components/maxWidthWrapper";
+import Phone from "@/components/phone";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-2 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:ob-52">
+          <div className="col-span-1 p-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                <Image
+                  width={70}
+                  height={70}
+                  src="/snake-1.png"
+                  alt="LOGo thingy I need to change"
+                />
+              </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your image on a{" "}
+                <span className="bg-red-600 px-2 text-white">Custom</span> Phone
+                Case
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Capture your worst nightmares with your own money{" "}
+                <span className="font-semibold">bitch</span> One-Wheel-Odyssy is
+                not only a useless one time purchase that you probably
+                won&apos;t ever use, it will also make you look like a complete
+                nonce, I mean who the fuck buys unicycles anymore
+              </p>
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <div className="wherethexissuppozedtobe"></div>
+                  <li className="flex gap-1.5 items-center text-left">
+                    Low-quality, cheap material
+                  </li>
+                </div>
+                <div className="space-y-2">
+                  <div className="wherethexissuppozedtobe"></div>
+                  <li className="flex gap-1.5 items-center text-left">
+                    4 year print nongarantee
+                  </li>
+                </div>
+                <div className="space-y-2">
+                  <div className="wherethexissuppozedtobe"></div>
+                  <li className="flex gap-1.5 items-center text-left">
+                    human legs not supported
+                  </li>
+                </div>
+              </ul>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5 ">
+                <div className="flex -space-x-4">
+                  <img
+                    src="/users/user-5.jpg"
+                    alt="unhappy customer's face"
+                    className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <img
+                    src="/users/user-1.png"
+                    alt="unhappy customer's face"
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <img
+                    src="/users/user-5.jpg"
+                    alt="unhappy customer's face"
+                    className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <img
+                    src="/users/user-1.png"
+                    alt="unhappy customer's face"
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                  />
+                </div>
+                <div className="flex flex-col justify-between items-center sm:items-start ">
+                  <div className="flex gap-0.5">
+                    <div className="h-4 w-4 bg-red-600" />
+                    <div className="h-4 w-4 bg-red-600" />
+                    <div className="h-4 w-4 bg-red-600" />
+                    <div className="h-4 w-4 bg-red-600" />
+                    <div className="h-4 w-4 bg-red-600" />
+                  </div>
+                  <p>
+                    <span className="font-semibold">1.250</span> unhappy
+                    customers
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="col-span-full h-[1500px] lg:col-span-1 w-full hidden lg:flex items-start justify-around px-4 sm:px-16 md:px-0  lg:mx-0 l ">
+            {/* <div className="relative md:max-w-xl">
+              <img
+                src="/your-image.png"
+                alt=""
+                className="absolute w-49 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
+              />
+              <img
+                src="/line.png"
+                alt=""
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+              />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
+            </div> */}
+            {/* <div className="h-[1000px] w-1/2 flex items-center justify-around"> */}
+            <div className="absolute w-[400px] overflow-hidden wrapper flex justify-center  ">
+              {[...Array(3)].map((_, i) => (
+                <div
+                  style={{
+                    animationDelay: `calc(10s / 3 * (${3 - i} - 1) * -1)`,
+                  }}
+                  className="item"
+                  key={i}
+                >
+                  <img
+                    src="/testimonials/1.jpg"
+                    alt="Hero Image"
+                    className="h-full w-full object-cover"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="absolute w-[400px] overflow-hidden wrapper  flex justify-center ">
+              {[...Array(3)].map((_, i) => (
+                <div
+                  style={{
+                    animationDelay: `calc(14s / 3 * (${3 - i} - 1) * -1)`,
+                  }}
+                  className="reverse-item"
+                  key={i}
+                >
+                  <img
+                    src="/testimonials/1.jpg"
+                    alt="Hero Image"
+                    className="h-full w-full object-cover"
+                  />
+                </div>
+              ))}
+            </div>{" "}
+          </div>
+          <style>{`
+        .marquee-container {
+          animation: marquee 12s linear infinite;
+        }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        @keyframes marquee {
+         0% {
+             transform: translateY(-100%);
+           }
+           100% {
+             transform: translateY(100%);
+           }
+        }
+.wrapper {
+     height: 90%;
+    max-height: 700px;
+    margin-inline: auto;
+    position: relative;
+    overflow: hidden;
+  mask-image: linear-gradient(
+    to top,
+    rgba(0, 0, 0, 0),
+    rgba(0, 0, 0, 1) 20%,
+    rgba(0, 0, 0, 1) 80%,
+    rgba(0, 0, 0, 0)
+  );
+}
+  
+.test {
+  background-color: green;
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+@keyframes scrollDown {
+  to {
+    Bottom: -200px;
+  }
+}
+
+@keyframes scrollUp {
+  to {
+    top: -200px;
+  }
+}
+.reverse-item {
+width: 250px;
+  height: 300px;
+  border-radius: 6px;
+  background-color: red;
+  position: absolute;
+  top: max(calc(300px * 3), 100%);
+  animation-name: scrollUp;
+  animation-duration: 14s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+}
+  .item {
+  width: 250px;
+  height: 300px;
+  border-radius: 6px;
+  background-color: red;
+  position: absolute;
+  Bottom: max(calc(300px * 3), 100%);
+  animation-name: scrollDown;
+  animation-duration: 10s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+} 
+
+  @keyframes scrollDown {
+      to {
+        bottom: -300px;
+      }
+    }
+.item1 {
+  animation-delay: calc(30s / 8 * (8 - 1) * -1);
+}
+
+.item2 {
+  animation-delay: calc(30s / 8 * (8 - 2) * -1);
+}
+
+.item3 {
+  animation-delay: calc(30s / 8 * (8 - 3) * -1);
+}
+
+.item4 {
+  animation-delay: calc(30s / 8 * (8 - 4) * -1);
+}
+
+.item5 {
+  animation-delay: calc(30s / 8 * (8 - 5) * -1);
+}
+
+.item6 {
+  animation-delay: calc(30s / 8 * (8 - 6) * -1);
+}
+
+.item7 {
+  animation-delay: calc(30s / 8 * (8 - 7) * -1);
+}
+
+.item8 {
+  animation-delay: calc(30s / 8 * (8 - 8) * -1);
+}
+        
+      `}</style>
+          {/* </div> */}
+        </MaxWidthWrapper>
+      </section>
+    </div>
   );
 }
