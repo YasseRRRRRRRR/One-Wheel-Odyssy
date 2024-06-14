923cbaf445358e1fba06cb84d86d910a3bfdcfd7
YasseRRRRRRRR <yassermoussaddak3@gmail.com>
2024-06-14 20:16:37 +0300

    the third section done. I think it look a lot worse compared to the high quality stuff above but I think maybe some framer motion animations will make it look better

--- one-wheel-odyssy/src/components/reviews.tsx
+++ one-wheel-odyssy/src/components/reviews.tsx
@@ -3,6 +3,7 @@ import MaxWidthWrapper from "./maxWidthWrapper";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 
 const Reviews = () => {
   return (
@@ -15,13 +16,8 @@ const Reviews = () => {
             <div className="mx-auto max-w-md text-center lg:text-left">
               <header>
                 <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
-                  Wether you are a hobbiest
+                  Whether you are a hobbiest
                 </h2>
-
-                {/* <p className="mt-4 text-gray-500">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
-                  rerum quam amet provident nulla error!
-                </p> */}
               </header>
             </div>
           </div>
@@ -29,37 +25,59 @@ const Reviews = () => {
           <div className="lg:col-span-2 lg:py-8">
             <ul className="grid grid-cols-2 gap-4">
               <li>
-                <a href="#" className="group block">
-                  <img
-                    src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80"
-                    alt=""
-                    className="aspect-square w-full rounded object-cover"
-                  />
+                <a href="#" className="group block overflow-hidden">
+                  <div className="relative h-[350px] sm:h-[450px]">
+                    <Image
+                      layout="fill"
+                      objectFit="cover"
+                      src="/unpaid-models-pleaseforgiveme/psychopath1.jpg"
+                      alt=""
+                      className="absolute inset-0 h-full w-full object-cover opacity-100 group-hover:opacity-0"
+                    />
+                    <Image
+                      layout="fill"
+                      objectFit="cover"
+                      src="/product-list/for-the-sociopaths.jpg"
+                      alt=""
+                      className="absolute inset-0 h-full w-full object-cover opacity-0 group-hover:opacity-100"
+                    />
+                  </div>
 
                   <div className="mt-3">
                     <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
-                      Simple Watch
+                      Club 20" 3-Wheeler Unicycle
                     </h3>
 
-                    <p className="mt-1 text-sm text-gray-700">$150</p>
+                    <p className="mt-1 text-sm text-gray-700">$500.00</p>
                   </div>
                 </a>
               </li>
 
               <li>
-                <a href="#" className="group block">
-                  <img
-                    src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80"
-                    alt=""
-                    className="aspect-square w-full rounded object-cover"
-                  />
+                <a href="#" className="group block overflow-hidden">
+                  <div className="relative h-[350px] sm:h-[450px]">
+                    <Image
+                      layout="fill"
+                      objectFit="cover"
+                      src="/unpaid-models-pleaseforgiveme/electrical-person.jpg"
+                      alt=""
+                      className="absolute inset-0 h-full w-full object-cover opacity-100 group-hover:opacity-0"
+                    />
+                    <Image
+                      layout="fill"
+                      objectFit="cover"
+                      src="/product-list/electrical-unicyle.jpg"
+                      alt=""
+                      className="absolute inset-0 h-full w-full object-cover opacity-0 group-hover:opacity-100"
+                    />
+                  </div>
 
                   <div className="mt-3">
                     <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
-                      Simple Watch
+                      16-inch Self-balance Electric Unicycle
                     </h3>
 
-                    <p className="mt-1 text-sm text-gray-700">$150</p>
+                    <p className="mt-1 text-sm text-gray-700">$299.66</p>
                   </div>
                 </a>
               </li>
@@ -73,11 +91,11 @@ const Reviews = () => {
             <div className="mx-auto max-w-md text-center lg:text-left">
               <header>
                 <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
-                  Or a professional
+                  Or a seasoned professional
                 </h2>
 
                 <p className="mt-4 text-gray-500">
-                  or maybe just bored we have you covered
+                  or maybe just bored we have got you covered
                 </p>
               </header>
               <div className="mt-8 hidden lg:inline-block">
@@ -87,54 +105,69 @@ const Reviews = () => {
                     size: "lg",
                   })}
                 >
-                  {/* "hidden lg:inline-block" */}
                   Get your customized unicycle
-                  {/* dakl */}
-                  {/* not sure if i should leave it or take it away */}
                   <ArrowRight className="ml-1.5 h-5 w-5" />
                 </Link>
               </div>
-              {/* <a
-                href="#"
-                className="hidden lg:inline-block mt-8  rounded border border-gray-900 bg-gray-900 px-12 py-3 text-sm font-medium text-white transition hover:shadow focus:outline-none focus:ring"
-              ></a> */}
             </div>
           </div>
 
           <div className="order-2 lg:order-1  lg:col-span-2 lg:py-8">
             <ul className="grid grid-cols-2 gap-4">
               <li>
-                <a href="#" className="group block">
-                  <img
-                    src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80"
-                    alt=""
-                    className="aspect-square w-full rounded object-cover"
-                  />
+                <a href="#" className="group block overflow-hidden">
+                  <div className="relative h-[350px] sm:h-[450px]">
+                    <Image
+                      layout="fill"
+                      objectFit="cover"
+                      src="/unpaid-models-pleaseforgiveme/vintage-professionals.jpg"
+                      alt=""
+                      className="absolute inset-0 h-full w-full object-cover opacity-100 group-hover:opacity-0"
+                    />
+                    <Image
+                      layout="fill"
+                      objectFit="cover"
+                      src="/product-list/larger-tire-model.jpg"
+                      alt=""
+                      className="absolute inset-0 h-full w-full object-cover opacity-0 group-hover:opacity-100"
+                    />
+                  </div>
 
                   <div className="mt-3">
                     <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
-                      Simple Watch
+                      24in unicycle with Alloy Rim-659340
                     </h3>
 
-                    <p className="mt-1 text-sm text-gray-700">$150</p>
+                    <p className="mt-1 text-sm text-gray-700">$141 38</p>
                   </div>
                 </a>
               </li>
 
               <li>
-                <a href="#" className="group block">
-                  <img
-                    src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80"
-                    alt=""
-                    className="aspect-square w-full rounded object-cover"
-                  />
+                <a href="#" className="group block overflow-hidden">
+                  <div className="relative h-[350px] sm:h-[450px]">
+                    <Image
+                      layout="fill"
+                      objectFit="cover"
+                      src="/unpaid-models-pleaseforgiveme/weirdly-shaped-unicycle.jpg"
+                      alt=""
+                      className="absolute inset-0 h-full w-full object-cover opacity-100 group-hover:opacity-0"
+                    />
+                    <Image
+                      layout="fill"
+                      objectFit="cover"
+                      src="/product-list/longer-pedals-model.jpg"
+                      alt=""
+                      className="absolute inset-0 h-full w-full object-cover opacity-0 group-hover:opacity-100"
+                    />
+                  </div>
 
                   <div className="mt-3">
                     <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
-                      Simple Watch
+                      Club 5-Foot Deluxe Giraffe
                     </h3>
 
-                    <p className="mt-1 text-sm text-gray-700">$150</p>
+                    <p className="mt-1 text-sm text-gray-700">$300.00</p>
                   </div>
                 </a>
               </li>
@@ -146,11 +179,7 @@ const Reviews = () => {
                   size: "lg",
                 })}
               >
-                {/* "hidden lg:inline-block" */}
                 Get your customized unicycle
-                {/* dakl */}
-                {/* not sure if i should leave it or take it away */}
-                {/* <ArrowRight />  */}
               </Link>
             </div>
           </div>
