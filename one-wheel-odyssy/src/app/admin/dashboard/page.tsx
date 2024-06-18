62b0948ab8a325ffe9d648070e03266f28c4575f
YasseRRRRRRRR <yassermoussaddak3@gmail.com>
2024-06-18 21:18:35 +0300

    why is this shit not working

--- one-wheel-odyssy/src/app/admin/dashboard/page.tsx
+++ one-wheel-odyssy/src/app/admin/dashboard/page.tsx
@@ -4,13 +4,29 @@ import {
   CardDescription,
   CardHeader,
 } from "@/components/ui/card";
+import db from "@/db/db";
 import React from "react";
-const getSalesData = () => {};
 
-const Page = () => {
+const getSalesData = async () => {
+  const data = await db.order.aggregate({
+    _sum: { priceInCents: true },
+    _count: true,
+  });
+  return {
+    amount: (data._sum.priceInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
+};
+
+const Page = async () => {
+  const salesData = await getSalesData();
   return (
     <div className="my-16 grid grid-cols-1 md:grid-cols-2 lg:grid-col-3 gap-4">
-      <DashboardCard title="Sales" subtitle="Test" body="body" />
+      <DashboardCard
+        title="Sales"
+        subtitle={salesData.numberOfSales}
+        body={salesData.amount}
+      />
       <DashboardCard title="Sales" subtitle="Test" body="body" />
       <DashboardCard title="Sales" subtitle="Test" body="body" />
     </div>
